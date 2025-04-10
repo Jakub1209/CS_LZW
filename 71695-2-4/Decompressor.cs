5aadc7bf5d521489cccd4384607ac54b4b9f6c38
Jakub <xxjakubszewczykxx@gmail.com>
2025-04-10 21:41:15 +0200

    The compressor and decompressor work correctly now!

---
+++ 71695-2-4/Decompressor.cs
@@ -0,0 +1,49 @@
+﻿namespace _71695_2_4;
+
+public class Decompressor
+{
+    public static List<string> Decompress(List<string> predefinedDictionary, List<int> compressedIndexes)
+    {
+        // initialize a list of strings for the decompressed result
+        List<string> decompressedResult = new List<string>();
+        // initialize a list containing the complete dictionary. This list will be constantly updated during the decompression
+        List<string> completeDictionary = predefinedDictionary;
+        // for each index in the compressedIndexes list, add the corresponding entry in the dictionary to the output
+        Console.WriteLine("Decompressing dictionary...");
+        for (int i = 0; i < compressedIndexes.Count; i++)
+        {
+            // keep track of the current index for the dictionary entry
+            int compressedIndex = compressedIndexes[i] - 1;
+            // keep track of the next compressed index.
+            int nextCompressedIndex = 0;
+            // initialize a string which will hold the new entry for the dictionary
+            string newDictionaryEntry = "";
+            // If there aren't any indexes, just add the current index and end the decompression
+            if (i + 1 < compressedIndexes.Count)
+            {
+                // if there isn't an entry in the dictionary with this current index
+                if (compressedIndexes[i + 1] > completeDictionary.Count)
+                    // the next compressed index should be the same as the current one
+                    nextCompressedIndex = compressedIndexes[i] - 1;
+                else
+                    // if it is, the next compressed index should be 0 based so we subtract one from the result
+                    nextCompressedIndex = compressedIndexes[i + 1] - 1;
+                // the next dictionary entry should be the current dictionary entry + the first character of the next entry
+                newDictionaryEntry = completeDictionary[compressedIndex] + completeDictionary[nextCompressedIndex][0];
+                // add new dictionary entry to the dictionary
+                completeDictionary.Add(newDictionaryEntry);
+            }
+            else
+            {
+                newDictionaryEntry = completeDictionary[compressedIndex];
+            }
+            
+            Console.WriteLine($"compressedIndex: {compressedIndexes[i]}, completeDictionary[base0compressedIndex]: {completeDictionary[compressedIndex]}, completeDictionary[nextCompressedIndex][0]: {completeDictionary[nextCompressedIndex][0]}, Added entry: {newDictionaryEntry}");
+            // add the dictionary entry to the result list
+            decompressedResult.Add(completeDictionary[compressedIndex]);
+            Console.WriteLine($"Decompressed entry: {completeDictionary[compressedIndex]}");
+        }
+        
+        return decompressedResult;
+    }
+}
--- 71695-2-4/Program.cs
+++ 71695-2-4/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        string input = "LATA_OSA_KOŁO_NOSA_LATA_MUCHA_KOŁO_UCHA";
-        
+        // string input = "LATA_OSA_KOŁO_NOSA_LATA_MUCHA_KOŁO_UCHA_LATA_BĄK_KOŁO_RĄK_LATA_OSA_KOŁO_NOSA_LATA_MUCHA_KOŁO_UCHA_LATA_BĄK_KOŁO_RĄK_LATA_OSA_KOŁO_NOSA_LATA_MUCHA_KOŁO_UCHA_LATA_BĄK_KOŁO_RĄK";
+        string input = "DBCDDCBBBCDBADBADACBCABACBACDCBCACDACADDBAAADBDCBDDDABACBCCAAACBCDBCBDADDBBBBCCBDDDBBAADDCDCCDADBDCDCCACADCDCAADDCDBAAABBACCDBDABBDCDBCCBCADDDDACCCCCBCBADDCDDCDBBCDCCBDCDBDABDBBDAABBAACACABDACAAADACAABDBCAABADCCADDBCACACBAACA";
         var (dictionary, indexes) = Compressor.GetFullDictionaryAndIndexes(input);
 
         Console.WriteLine("dictionary:");
@@ -18,6 +18,25 @@ class Program
         foreach (int i in indexes)
         {
             Console.WriteLine($"index {i}, dictionaryEntry: {dictionary[i-1]}");
+        }
+        
+        List<string> predefinedDictionary = Compressor.CreatePredefinedDictionary(input);
+        
+        List<string> decompressedResult = Decompressor.Decompress(predefinedDictionary, indexes);
+        string result = string.Join("", decompressedResult);
+        Console.Write("decompressedResult: ");
+        foreach (string s in decompressedResult)
+        {
+            Console.Write(s);
+        }
+        Console.WriteLine();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] != result[i])
+            {
+                Console.Write($"There character on index {i} shoulbe be {input[i]}, not {result[i]}!");
+            }
         }
     }
 }
