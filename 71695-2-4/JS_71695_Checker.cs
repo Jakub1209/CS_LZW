70d997a28b09817c7fcee9b07a8874d0f46e3b5e
Jakub <kubzooone@gmail.com>
2025-04-11 15:38:33 +0200

    User interaction for the program added

---
+++ 71695-2-4/JS_71695_Checker.cs
@@ -0,0 +1,93 @@
+﻿namespace _71695_2_4;
+
+public class JS_71695_Checker
+{
+    // check is the inputted string is valid for compression
+    public static bool JS_71695_IsValidForCompression(string JS_71695_input)
+    {
+        // the string cannot be null, or be all made from white spaces
+        if (!JS_71695_NoNullOrWhiteSpace(JS_71695_input)) return false;
+        // the string cannot be too short or too long
+        if (!JS_71695_LengthIsValid(JS_71695_input)) return false;
+        // the string cannot contain control characters
+        if (!JS_71695_NoControlCharacters(JS_71695_input)) return false;
+        // if everything seems fine, accept the string for compression
+        return true;
+    }
+
+    static bool JS_71695_NoControlCharacters(string JS_71695_input)
+    {
+        // if any of the chars entered into the string are a control character, but it isn't a white space,
+        // reject that string
+        if (JS_71695_input.Any(c => char.IsControl(c) && !char.IsWhiteSpace(c)))
+        {
+            Console.WriteLine("The string shouldn't contain control characters like these:\n" +
+                              "- Null (0x00)\n" +
+                              "- Start of Heading (0x01)\n" +
+                              "- Start of Text (0x02)\n" +
+                              "- End of Text (0x03)\n" +
+                              "- Backspace (0x08)\n" +
+                              "- Horizontal Tab (0x09)\n" +
+                              "- Line Feed (0x0A)\n" +
+                              "- Vertical Tab (0x0B)\n" +
+                              "- Form Feed (0x0C)\n" +
+                              "- Carriage Return (0x0D)\n" +
+                              "- Escape (0x1B)\n" +
+                              "- Delete (0x7F)\n" +
+                              "- Please, try again!");
+            return false;
+        }
+        return true;
+    }
+
+    static bool JS_71695_LengthIsValid(string JS_71695_input)
+    {
+        // hard-code the min and max length a string should be
+        int JS_71695_minLength = 10;  // Too short to benefit from compression
+        int JS_71695_maxLength = 1_000_000;  // Prevent denial-of-service
+        // check if the length requirements are met
+        if (JS_71695_input.Length < JS_71695_minLength || JS_71695_input.Length > JS_71695_maxLength)
+        {
+            Console.WriteLine($"The minimum string length should be: {JS_71695_minLength} and the max length: " +
+                              $"{JS_71695_maxLength}.\n Your is: {JS_71695_input.Length}. Please, try again!");
+            return false;
+        }
+        return true;
+    }
+
+    static bool JS_71695_NoNullOrWhiteSpace(string JS_71695_input)
+    {
+        if (string.IsNullOrWhiteSpace(JS_71695_input))
+        {
+            Console.WriteLine("The string cannot be null or empty. Please, try again!");
+            // reject that string
+            return false;
+        }
+        return true;
+    }
+    
+    // check if the string has been decompressed correctly, by analyzing each of the characters in the input and output strings
+    public static void JS_71695_CheckIfDecompressedCorrectly(string JS_71695_input, string JS_71695_output)
+    {
+        // for every character in the input string,
+        for (int JS_71695_i = 0; JS_71695_i < JS_71695_input.Length; JS_71695_i++)
+        {
+            // check if the character at a specified index is the same as the character on the same index in the output string
+            // if not, write where the problem is
+            if (JS_71695_input[JS_71695_i] != JS_71695_output[JS_71695_i])
+            {
+                Console.WriteLine($"The character on index {JS_71695_i}: '{JS_71695_input[JS_71695_i]}' is not the same as the character on the output '{JS_71695_output[JS_71695_i]}'");
+            }
+        }
+
+        // if the input string is equal to the output string, note that the string has been correctly compressed
+        if (JS_71695_input == JS_71695_output)
+        {
+            Console.WriteLine("Compressed Correctly!");
+        }
+        else
+        {
+            Console.WriteLine("Decompressed Incorrectly!");
+        }
+    }
+}
---
+++ 71695-2-4/UserInteraction.cs
@@ -0,0 +1,193 @@
+﻿namespace _71695_2_4;
+
+public class UserInteraction
+{
+    public static void GreetUser()
+    {
+        Console.WriteLine("Hello and welcome to LZW compression and decompression program!\n");
+    }
+
+    public static void RunProgram()
+    {
+        do
+        {
+            // initialize an empty string to keep the user input
+            string input;
+            // until the user enters a valid string, ask him to enter a response
+            do
+            {
+                Console.WriteLine("Enter your input of choice to compress into the field below: ");
+                input = Console.ReadLine();
+                Console.WriteLine();
+            }
+            // check if the input from the user is valid
+            while (!JS_71695_Checker.JS_71695_IsValidForCompression(input));
+
+            // compress the string for the user and ask if the user wants to see the results
+            InteractionForCompression(input);
+        } 
+        while (UserWantsToUseProgram());
+    }
+
+    static bool UserWantsToUseProgram()
+    {
+        bool runProgram = InteractionForRunningProgram();
+        
+        return runProgram;
+    }
+
+    static bool InteractionForRunningProgram()
+    {
+        Console.WriteLine("Would you like to use the program again? (Y/N): ");
+        while (true)
+        {
+            ConsoleKey key = Console.ReadKey(true).Key;
+            
+            if (key == ConsoleKey.Y)
+            {
+                Console.WriteLine("I'm so glad! Let's do compressing once again!");
+                Console.WriteLine();
+                break;
+            }
+
+            if (key == ConsoleKey.N)
+            {
+                Console.WriteLine("I've worked so hard on this algorithm... well...");
+                Console.WriteLine();
+                break;
+            }
+            
+            Console.WriteLine("You have pressed an incorrect key! Please try again (Y/N): ");
+        }
+        return true;
+    }
+
+    static void InteractionForCompression(string input)
+    {
+        // get the list of compressed indexes
+        List<int> indexes = Compressor.GetCompressedIndexesList(input);
+        // get the predefined dictionary containing each unique letter in the string
+        List<string> predefinedDictionary = Dictionary.CreatePredefinedDictionary(input);
+        // manage the users interaction when prompted with the compressed result
+        InteractionAfterCompression(ref indexes, ref predefinedDictionary);
+        // ask the user if he would like to decompress the string and later check for differences
+        InteractionForDecompression(ref indexes, ref predefinedDictionary, ref input);
+    }
+
+    static void InteractionForDecompression(ref List<int> indexes, ref List<string> predefinedDictionary, ref string input)
+    {
+        Console.WriteLine("Would you like to use the decompression algorithm? (Y/N): ");
+        while (true)
+        {
+            ConsoleKey key = Console.ReadKey(true).Key;
+            
+            if (key == ConsoleKey.Y)
+            {
+                // decompress the input and save it into this list
+                List<string> decompressedResult = Decompressor.Decompress(predefinedDictionary, indexes);
+                // print out the decompressed result
+                Console.Write("Decompressed string: ");
+                foreach (string s in decompressedResult)
+                {
+                    Console.Write(s);
+                }
+                Console.WriteLine("\n");
+                // ask the user if he would like to validate the result of the decompression
+                InteractionAfterDecompression(ref input, ref decompressedResult);
+                // exit the while loop
+                break;
+            }
+
+            if (key == ConsoleKey.N)
+            {
+                Console.WriteLine("I've worked so hard on this algorithm... well...");
+                Console.WriteLine();
+                break;
+            }
+            
+            Console.WriteLine("You have pressed an incorrect key! Please try again (Y/N): ");
+        }
+    }
+
+    static void InteractionAfterDecompression(ref string input, ref List<string> decompressedResult)
+    {
+        Console.WriteLine("Would you like to check if the decompression was successful? (Y/N): ");
+        while (true)
+        {
+            ConsoleKey key2 = Console.ReadKey(true).Key;
+    
+            if (key2 == ConsoleKey.Y)
+            {
+                Console.WriteLine("Side by side comparison of characters:");
+                for (int i = 0; i < input.Length; i++)
+                {
+                    Console.WriteLine($"char index: {i}, input char: {input[i]}, output char: {string.Join("", decompressedResult)[i]}");
+                }
+
+                Console.WriteLine();
+                if (input != string.Join("", decompressedResult))
+                {
+                    Console.WriteLine("There are differences in the string!");
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        if (input[i] != string.Join("", decompressedResult)[i])
+                        {
+                            Console.WriteLine($"char index: {i}, input char: {input[i]}, output char {string.Join("", decompressedResult)[i]}");
+                        }
+                    }
+                }
+                else Console.WriteLine("There are no differences! Compression and decompression successful!");
+                Console.WriteLine();
+                // exit the while loop
+                break;
+            }
+
+            if (key2 == ConsoleKey.N)
+            {
+                Console.WriteLine("Oh you...");
+                Console.WriteLine();
+                break;
+            } 
+            Console.WriteLine("You have pressed an incorrect key! Please try again (Y/N): ");
+        }
+    }
+
+    static void InteractionAfterCompression(ref List<int> indexes, ref List<string> predefinedDictionary)
+    {
+        Console.WriteLine("Your string has been compressed! Would you like to see the results? (Y/N): ");
+        while (true)
+        {
+            ConsoleKey key = Console.ReadKey(true).Key;
+            
+            if (key == ConsoleKey.Y)
+            {
+                // print out the indexes list
+                Console.Write("compressed indexes list: [");
+                for (int i = 0; i < indexes.Count; i++)
+                {
+                    if (i == indexes.Count - 1) Console.WriteLine($"{indexes[i]}]");
+                    else Console.Write($"{indexes[i]}, ");
+                }
+                // print out the dictionary
+                Console.Write("predefined dictionary: [");
+                for (int i = 0; i < predefinedDictionary.Count; i++)
+                {
+                    if (i == predefinedDictionary.Count - 1) Console.WriteLine($"{predefinedDictionary[i]}]");
+                    else Console.Write($"{predefinedDictionary[i]}, ");
+                }
+
+                Console.WriteLine();
+                // exit the while loop
+                break;
+            }
+
+            if (key == ConsoleKey.N)
+            {
+                Console.WriteLine("You don't want to see the result of my work? Aww...");
+                Console.WriteLine();
+                break;
+            }
+            Console.WriteLine("You have pressed an incorrect key! Please try again (Y/N): ");
+        }
+    }
+}
